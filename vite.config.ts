7fd9e1a4503a1a6aa2f99f8dd845231a1697fbbe
Hy Huynh <giahy33@gmail.com>
2025-03-27 11:49:02 -0600

    feat: add initial Cloudflare Worker setup with configuration and update Vite integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import svgLoader from 'vite-svg-loader';
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
         svgLoader(),
+        cloudflare(),
     ],
     server: {
         port: 3000,
